b6929832a33d6282c7f0ba0336e5dc56e3ee3c89
Wibus <1596355173@qq.com>
2022-03-04 23:21:10 +0800

    perf(widget): limit image size in Widget component

--- components/Widget.tsx
+++ components/Widget.tsx
@@ -1,18 +1,17 @@
 /*
- * @FilePath: /GS-web-demo/components/Widget.tsx
+ * @FilePath: /gs-web-demo/components/Widget.tsx
  * @author: Wibus
  * @Date: 2022-02-27 20:05:02
  * @LastEditors: Wibus
- * @LastEditTime: 2022-02-27 23:19:32
+ * @LastEditTime: 2022-03-04 23:10:54
  * Coding With IU
  */
-import { ReactChild, ReactFragment, ReactPortal } from 'react';
 import styles from '../styles/Home.module.css'
 
 export const Widget = (props: any) => {
   return (
     <div className={styles.widget}>
-      <img src={props.img} alt="{props.img}" />
+      <img src={props.img} alt={props.img} style={{zoom: 10}} />
       <a href={props.href}>{props.title}</a>
       <p>{props.description}</p>
     </div>
