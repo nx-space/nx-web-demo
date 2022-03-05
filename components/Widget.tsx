889fd7b6818f712229f5acf5859b1b0253324d74
wibus-wee <1596355173@qq.com>
2022-03-05 23:24:08 +0800

    fix(components): style grammer is wrong

--- components/Widget.tsx
+++ components/Widget.tsx
@@ -1,9 +1,9 @@
 /*
- * @FilePath: /gs-web-demo/components/Widget.tsx
+ * @FilePath: /GS-web-demo/components/Widget.tsx
  * @author: Wibus
  * @Date: 2022-02-27 20:05:02
  * @LastEditors: Wibus
- * @LastEditTime: 2022-03-04 23:10:54
+ * @LastEditTime: 2022-03-05 07:16:06
  * Coding With IU
  */
 import styles from '../styles/Home.module.css'
@@ -11,7 +11,7 @@ import styles from '../styles/Home.module.css'
 export const Widget = (props: any) => {
   return (
     <div className={styles.widget}>
-      <img src={props.img} alt={props.img} style={{zoom: 10}} />
+      <img src={props.img} alt={props.img} style={{width: "inherit", zoom: "5%"}} />
       <a href={props.href}>{props.title}</a>
       <p>{props.description}</p>
     </div>
